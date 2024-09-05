67e37851351c51d51e520a83a8a5e45f60653d9b
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-09-05 23:50:54 +0800

    implement layout.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 
 import styled from "styled-components";
+import { MainLayout } from "./styles/Layouts";
+
 
 function App() {
-
-  const AppStyled = styled.div`
-  height: 100vh;
-  `;
-
   return (
     <AppStyled className="App">
-
+      <MainLayout>
+        <h1>
+          Hello
+        </h1>
+      </MainLayout>
     </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-color: #8BC6EC;
+  background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
+
+`;
 
 
 export default App;
